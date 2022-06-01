c8e3241170d2098a77f1c938c9c702bdb9f94ed5
ihor <igchup1985@gmail.com>
2022-06-01 23:43:50 +0300

    behaviorSubject

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+import {IUser} from "../../interfaces";
+import {DataService} from "../../services";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  @Input()
+  user: IUser
+
+  constructor(private dataService:DataService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  saveToStorage() {
+    this.dataService.userStorage.next(this.user)
+  }
+}
