e9009a680651168eeff37d2eb4a17bb999d77562
ihor <igchup1985@gmail.com>
2022-06-01 23:43:28 +0300

    behaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "./services";
+import {IUser} from "./interfaces";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+  user: IUser
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit(): void {
+    this.dataService.userStorage.subscribe(value => this.user = value)
+  }
 }
